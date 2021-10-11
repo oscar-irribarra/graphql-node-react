c0df8ddad4c384b86d5c5eac0241b5c994a3ad6c
Oscar Irribarra <osc-r@live.cl>
2021-10-10 23:06:12 -0300

    se agrega endpoints eventos y usuarios, se hace conexion con mongoDB

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
-
-const events = [];
 
 app.use(express.json());
 
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: buildSchema(`
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
         type Event {
             _id: ID!
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
         }
 
         input EventInput {
@@ -27,12 +37,18 @@ app.use(
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
         }
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -40,23 +56,82 @@ app.use(
             mutation: RootMutation
         }
     `),
-    rootValue: {
-      events: () => {
-        return events;
-      },
-      createEvent: (args) => {
-        const event = { ...args };
-        event.date = new Date().toString();
-        event._id = Math.random().toString();
+        rootValue: {
+            events: async () => {
+                try {
+                    return await Event.find();
+                } catch (err) {
+                    console.log(err);
+                    throw err;
+                }
+            },
+            createEvent: async (args) => {
+                try {
+                    const event = new Event({
+                        title: args.eventInput.title,
+                        description: args.eventInput.description,
+                        price: +args.eventInput.price,
+                        date: new Date(args.eventInput.date),
+                        creator: '6163914dbb0d51a3cc494770'
+                    });
+                    const createdEvent = await event.save();
 
-        events.push(event);
-        return event;
-      }
-    },
-    graphiql: true
-  })
+                    const user = await User.findById(
+                        '6163914dbb0d51a3cc494770'
+                    );
+                    if (!user) {
+                        throw new Error('User not found');
+                    }
+
+                    user.createdEvents.push(event);
+                    await user.save();
+
+                    return createdEvent;
+                } catch (err) {
+                    throw err;
+                }
+            },
+            createUser: async (args) => {
+                try {
+                    const isEmailExist = await User.findOne({
+                        email: args.userInput.email
+                    });
+
+                    if (isEmailExist) {
+                        throw new Error('User already exist');
+                    }
+
+                    const hashedPassword = await bcrypt.hashSync(
+                        args.userInput.password,
+                        12
+                    );
+
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword
+                    });
+
+                    const userReturn = await user.save();
+                    userReturn.password = null;
+                    return userReturn;
+                } catch (err) {
+                    throw err;
+                }
+            }
+        },
+        graphiql: true
+    })
 );
 
-app.listen(3000, () => {
-  console.log('escuchando puerto 3000..');
-});
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.axare.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
+    )
+    .then(
+        app.listen(3000, () => {
+            console.log('escuchando puerto 3000..');
+        })
+    )
+    .catch((err) => {
+        console.log(err);
+    });
