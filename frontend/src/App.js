e494bc2b19654e37bdc66011544becb94dd17dc6
Oscar Irribarra <osc-r@live.cl>
2021-10-20 20:08:40 -0300

    se agrega navegacion y componente navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { Auth } from './pages/Auth';
+import { Events } from './pages/Events';
+import { Booking } from './pages/Booking';
+import { MainNavigation } from './components/navigation/MainNavigation';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
+        <BrowserRouter>
+            <MainNavigation />
+            <main className="main-content">
+                <Switch>
+                    <Redirect from="/" to="/auth" exact />
+                    <Route path="/auth" component={Auth} />
+                    <Route path="/events" component={Events} />
+                    <Route path="/bookings" component={Booking} />
+                </Switch>
+            </main>
+        </BrowserRouter>
     );
 }
 
