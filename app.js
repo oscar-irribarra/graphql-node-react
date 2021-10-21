4e2669c78e84234df36937834fffa97d34e71c96
Oscar Irribarra <osc-r@live.cl>
2021-10-20 22:55:26 -0300

    se implementa logIn y signUp desde cliente y se agrega middleware CORS a backend

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const mongoose = require('mongoose');
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
+const enableCORS = require('./middleware/enable-cors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(enableCORS);
 
 app.use(isAuth);
 
@@ -27,8 +30,8 @@ mongoose
         `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.axare.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
     )
     .then(
-        app.listen(3000, () => {
-            console.log('escuchando puerto 3000..');
+        app.listen(8000, () => {
+            console.log('escuchando puerto 8000..');
         })
     )
     .catch((err) => {
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Auth.css';
 
 export const Auth = () => {
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [isLogin, setIsLogin] = useState(true);
+
+    const isInvalid =
+        password.trim().length === 0 || emailAddress.trim().length === 0;
+
+    const switchModehandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+
+        const logInQuery = {
+            query: `
+                query{
+                    login(email: "${emailAddress}" password: "${password}"){
+                        token
+                    }
+                }
+            
+            `
+        };
+
+        const signUpQuery = {
+            query: `
+                mutation {
+                    createUser(userInput: { email:"${emailAddress}", password:"${password}" }){
+                        _id,
+                        email
+                    }
+                }
+            `
+        };
+
+        try {
+            const response = await fetch('http://localhost:8000/graphql', {
+                method: 'POST',
+                body: JSON.stringify(isLogin ? logInQuery : signUpQuery),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.status !== 200 && response.status !== 201) {
+                console.error('Failed!');
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (err) {
+            // throw new Error(err);
+            console.error(err);
+        }
+    };
+
     return (
-        <div>
-            <h1>Auth Page</h1>
-        </div>
+        <form onSubmit={submitHandler} method="POST" className="auth-form">
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={emailAddress}
+                    onChange={({ target }) => {
+                        setEmailAddress(target.value);
+                    }}
+                />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={({ target }) => {
+                        setPassword(target.value);
+                    }}
+                />
+            </div>
+            <div className="form-actions">
+                <button disabled={isInvalid} type="submit">
+                    {isLogin ? 'Log In' : 'Sign Up'}
+                </button>
+                <button type="button" onClick={switchModehandler}>
+                    Go to {!isLogin ? 'Log In' : 'Sign Up'}
+                </button>
+            </div>
+        </form>
     );
 };
