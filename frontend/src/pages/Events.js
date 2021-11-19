dfcaf02dc75611deb538519ccdbf0dccc15c918f
Oscar Irribarra <osc-r@live.cl>
2021-11-18 21:38:36 -0300

    se modifican estilos de header y se agrega modal de creacion

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Modal from '../components/modal/Modal';
+import Backdrop from '../components/backdrop/Backdrop';
+import './Events.css';
 
 const Events = () => {
+    const [creating, setCreating] = useState(false);
+
+    const startCreateEventHandler = () => {
+        setCreating(true);
+    };
+
+    const modalConfirmHandler = () => {
+        setCreating(false);
+    };
+
+    const modalCancelHandler = () => {
+        setCreating(false);
+    };
+
     return (
-        <div>
-            <h1>Events Page</h1>
-        </div>
+        <>
+            {creating && <Backdrop></Backdrop>}
+            {creating && (
+                <Modal
+                    title="Add Event"
+                    canCancel
+                    canConfirm
+                    onCancel={modalCancelHandler}
+                    onConfirm={modalConfirmHandler}
+                >
+                    <p>Modal content</p>
+                </Modal>
+            )}
+            <div className="events-control">
+                <p>Share your event</p>
+                <button className="btn" onClick={startCreateEventHandler}>
+                    Create Event
+                </button>
+            </div>
+        </>
     );
 };
 
